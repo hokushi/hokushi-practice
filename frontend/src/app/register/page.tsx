21973e9b0c65bec3cc72f6761fad840a88c392c1
hokushi <hokushi97@gmail.com>
2025-09-02 21:53:20 +0900

    prettier-plugin-tailwindcss導入

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Register() {
-  return <div>hoge</div>;
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="h-1/2 w-1/2 shadow">hoge</div>
+    </div>
+  );
 }
