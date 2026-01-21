82af6e0c901f8ee6d41e16d28bd12ab145560a96
hokushi <hokushi97@gmail.com>
2026-01-21 22:17:32 +0900

    ディレクトリ名変更

--- frontend/src/app/(post-login)/page.tsx
+++ frontend/src/app/(post-login)/page.tsx
@@ -1,5 +1,5 @@
 import { cookies } from "next/headers";
-import UserNameDisplay from "./top/logout/UserNameDisplay";
+import UserNameDisplay from "./home/logout/UserNameDisplay";
 
 const API_BASE_URL = process.env.API_BASE_URL ?? "http://localhost:3002";
 
