0af897e8538cf499ff508ecdb41732454f500853
hokushi <hokushi97@gmail.com>
2026-01-21 20:29:57 +0900

    スタイル調整

--- frontend/src/app/(post-login)/Sidebar.tsx
+++ frontend/src/app/(post-login)/Sidebar.tsx
@@ -3,7 +3,7 @@ import PlusIcon from "../components/icons/PlusIcon";
 
 export default function Sidebar() {
   return (
-    <aside className="flex w-[15%] min-w-[160px] flex-col border-r bg-slate-50 p-4">
+    <aside className="flex w-[15%] flex-col border-r bg-slate-50 p-4">
       <Button type="button" variant="outline" className="w-full justify-start">
         <PlusIcon className="size-6" />
         <span>新規作成</span>
